86e844dbd10db61ebbf7611ab4343cc515148fa0
GingerTC <72882312+GingerTC@users.noreply.github.com>
2020-12-16 09:55:44 -0800

    import sqlalchemy

--- view.py
+++ view.py
@@ -1,5 +1,3 @@
 from flask import Flask
-from sqlalchemy import func
-from pythondb import pythondb_bp
-from pythondb.model import Users, Usernames, Passwords
-from __init__ import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, inspect
