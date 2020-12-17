3f175344cb2df50342e2cb9d3e15ff9fcd081809
GingerTC <72882312+GingerTC@users.noreply.github.com>
2020-12-17 09:43:58 -0800

    added a base for what grid should look like

--- view.py
+++ view.py
@@ -1,3 +1,13 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, inspect
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route ("/grid")
+def grid():
+    return render_template('grid.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
