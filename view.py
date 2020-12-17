a14c4b311563787b6b7234528969e407363a81e2
dylanroman <dylanroman143@gmail.com>
2020-12-17 10:12:17 -0800

    Added home.html, base.html, and musicgrid.html Created app routes for html files Created content blocks for future use

--- view.py
+++ view.py
@@ -1,11 +1,25 @@
+#imports
 from flask import Flask, render_template
+import sqlalchemy
 
-
+#create flask
 app = Flask(__name__)
 
+#app routes
+##temp
 @app.route ("/grid")
 def grid():
     return render_template('grid.html')
+
+##home
+@app.route ("/")
+def home():
+    return render_template('home.html')
+
+##music grid
+@app.route ("music-maker")
+def grid():
+    return render_template('musicgrid')
 
 
 
