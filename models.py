b8815b5111f317c0b82bfd6eacea3eab7a90b09a
dylanroman <dylanroman143@gmail.com>
2020-12-20 02:40:58 -0800

    created navigation bar called navigation bar to all other files created database created "USER" class for database created models.py for database models in future

---
+++ models.py
@@ -0,0 +1,8 @@
+#imports
+from view import db
+
+#models
+class USER(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(15), unique = True, nullable = False)
+    password = db.Column(db.String(60), nullable = False)
--- view.py
+++ view.py
@@ -1,9 +1,17 @@
 #imports
 from flask import Flask, render_template
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from models import USER
 
 #create flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "", static_folder = "static")
+
+#database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+def __repr__(self):
+    return f"User('{self.username}')"
 
 #app routes
 ##temp
@@ -17,11 +25,14 @@ def home():
     return render_template('home.html')
 
 ##music grid
-@app.route ("music-maker")
-def grid():
-    return render_template('musicgrid')
+@app.route ("/music-maker")
+def musicgrid():
+    return render_template('musicgrid.html')
 
+@app.route ("/login")
+def login():
+    return render_template('account.html')
 
-
+#run
 if __name__ == "__main__":
     app.run(debug = True)
